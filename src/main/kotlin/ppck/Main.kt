cad01e03b09dce5e975a4301aa5d83b1256c8247
serhadtekkol <bahadir_tekkol@hotmail.com>
2022-06-02 20:33:39 +0300

    Initial commit

---
+++ src/main/kotlin/ppck/Main.kt
@@ -0,0 +1,27 @@
+
+fun main() {
+    println("Hello World!")
+//todo naber lan
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+  /*val değiştirilemez value
+  var değiştirilebilir variable
+  mutable değişebilir değişken
+  immutable değişmez değişken
+  kotlinde primitive değişken tanımlanabilir integer gibi
+
+   */
+    val name="Serhad"
+    val names="tekkol"
+val age=32
+
+val skintone="white"
+
+
+    println(name)
+
+    println(names)
+    println(skintone)
+    println(age)
+
+}
